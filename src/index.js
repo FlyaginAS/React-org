684487f2de9062d75978d651d95225481dc852e1
ruscat <rus.reg.36@gmail.com>
2019-12-04 17:10:42 +0300

    React+Redux/03/todo

--- src/index.js
+++ src/index.js
@@ -1,64 +1,6 @@
 import  React from 'react';
 import ReactDOM from 'react-dom';
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-class TodoApp extends React.Component{
-    constructor(props){
-        super(props);
-        this.state= {items: [], text: ''};
-        this.handleChange=this.handleChange.bind(this);
-        this.handleSubmit  = this.handleSubmit.bind(this);
-    }
-    handleSubmit(e){
-        e.preventDefault();
-        if(this.state.text.length ===0){
-            return;
-        }
-        const newItem={
-            text: this.state.text,
-            id: Date.now(),
-        };
-        this.setState(state=>({
-            items: state.items.concat(newItem),
-            text: '',
-        }));
-    }
-    handleChange(e){
-        this.setState({text: e.target.value});
-    }
-    render(){
-        return (
-            <div>
-                <h3>Список дел</h3>
-                <TodoList items={this.state.items} />
-                <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="new-todo">
-                        Что нужно сделать?
-                    </label>
-                    <input
-                        type="text"
-                        id='new-todo'
-                        value={this.state.text}
-                        onChange={this.handleChange}
-                    />
-                    <button>
-                        Добавить №{this.state.items.length + 1}
-                    </button>
-                </form>
-            </div>
-        );
-    }
-}
-class TodoList extends React.Component{
-    render(){
-        return (
-            <ul>
-                {this.props.items.map(item=> (
-                    <li key={item.id}>{item.text}</li>
-                ))}
-            </ul>
-        );
-    }
-}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ReactDOM.render(
