297c70dcc668884e945cf7508a76f45feab5b1bb
ruscat <rus.reg.36@gmail.com>
2019-12-04 12:36:57 +0300

    переносим состояние в боард

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import  React from 'react';
 import  ReactDOM from 'react-dom';
 import  './index.css';
-
-
+//
 class Square extends React.Component {
-
+    constructor(props){
+        super(props);
+        this.state={value: null,};
+    }
     render() {
         return (
-            <button
-                className="square"
-                onClick={()=> this.props.onClick()}
-            >
-                {this.props.value}
+            <button className="square" onClick={()=>this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
@@ -20,20 +19,13 @@ class Square extends React.Component {
 class Board extends React.Component {
     constructor(props){
         super(props);
-        this.state= {
+        this.state={
             squares: Array(9).fill(null),
-        };
+        }
     }
-    handleClick(i){
-        const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
-    }
+
     renderSquare(i) {
-        return (<Square
-            value={this.state.squares[i]}
-            onClick={()=> this.handleClick(i)}
-        />);
+        return <Square value={i} />;
     }
 
     render() {
@@ -84,3 +76,4 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
