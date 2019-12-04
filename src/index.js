42f1d44402118a4d91d57727952d14338a3f1412
ruscat <rus.reg.36@gmail.com>
2019-12-04 16:23:08 +0300

    написал todoapp на реакт

--- src/index.js
+++ src/index.js
@@ -1,34 +1,67 @@
 import  React from 'react';
 import ReactDOM from 'react-dom';
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-
-class Timer extends React.Component{
+class TodoApp extends React.Component{
     constructor(props){
         super(props);
-        this.state={seconds: 0};
+        this.state= {items: [], text: ''};
+        this.handleChange=this.handleChange.bind(this);
+        this.handleSubmit  = this.handleSubmit.bind(this);
     }
-    tick(){
-        this.setState((state)=>({
-            seconds: state.seconds+1
+    handleSubmit(e){
+        e.preventDefault();
+        if(this.state.text.length ===0){
+            return;
+        }
+        const newItem={
+            text: this.state.text,
+            id: Date.now(),
+        };
+        this.setState(state=>({
+            items: state.items.concat(newItem),
+            text: '',
         }));
     }
-    componentDidMount(){
-        this.timerId =  setInterval(()=> this.tick(), 1000);
-    }
-    componentWillUnmount(){
-        clearInterval(this.timerId);
+    handleChange(e){
+        this.setState({text: e.target.value});
     }
     render(){
         return (
             <div>
-                Seconds: {this.state.seconds}
+                <h3>Список дел</h3>
+                <TodoList items={this.state.items} />
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="new-todo">
+                        Что нужно сделать?
+                    </label>
+                    <input
+                        type="text"
+                        id='new-todo'
+                        value={this.state.text}
+                        onChange={this.handleChange}
+                    />
+                    <button>
+                        Добавить №{this.state.items.length + 1}
+                    </button>
+                </form>
             </div>
+        );
+    }
+}
+class TodoList extends React.Component{
+    render(){
+        return (
+            <ul>
+                {this.props.items.map(item=> (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ul>
         );
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ReactDOM.render(
-    <Timer/>,
+    <TodoApp/>,
     document.querySelector('#root')
 );
